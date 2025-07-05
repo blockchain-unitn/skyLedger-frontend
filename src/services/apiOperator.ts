04bc6d0db0ccaf0e43df1a583bf9a34e3fc492e0
siegluke <luca.claus02@gmail.com>
2025-07-05 11:32:09 +0200

    Update apiOperator.ts

--- src/services/apiOperator.ts
+++ src/services/apiOperator.ts
@@ -3,18 +3,15 @@ import { Operator } from '@/types/apiTypes';
 
 const API_BASE_URL = 'http://localhost:3000';
 
-// Lista di indirizzi da interrogare
-const addressList = [
-  "0x54B827DF9fc7c072Acff349a9A007EC49e473215",
-  "0x112F40B5Db93ceCe802F960Aa36DF1E0DB31e7dB",
-
-];
-
 export const getOperators = async (): Promise<Operator[]> => {
+  // 1. Prendi la lista degli address
+  const allResponse = await axios.get(`${API_BASE_URL}/api/operators/all`);
+  const addressList: string[] = allResponse.data.data;
+
+  // 2. Per ogni address, prendi i dettagli
   const results = await Promise.all(
     addressList.map(async (address) => {
       const response = await axios.get(`${API_BASE_URL}/api/operators/info/${address}`);
-      // Adatta questa parte in base alla struttura della risposta dell'API
       const data = response.data.data;
       return {
         address: data.address,
